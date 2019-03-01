116c78acfa60996e06cd2f879fea800927375c52
PavelLikharev <pavel7321@mail.ru>
2019-03-01 15:33:06 +0700

    Закончил проект

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 var button = document.querySelector(".button-call-form");
-
-var form = document.querySelector(".container-form");
+var search = document.querySelector(".container-form");
+var arrival = search.querySelector("[name=date-arrival]");
+var form = search.querySelector("form");
+var out = search.querySelector("[name=date-out]");
+var adults = search.querySelector("[name=adults]");
+var children = search.querySelector("[name=children]");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  form.classList.toggle("container-form-show");
+  search.classList.toggle("container-form-show");
+  arrival.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!arrival.value || !out.value || !adults.value || !children.value) {
+      evt.preventDefault();
+      search.classList.remove("form-error");
+      search.offsetWidth = search.offsetWidth;
+      search.classList.add("form-error");
+}
 });
